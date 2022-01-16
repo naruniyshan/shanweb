f9499b2fa6ad25b0957b6d4e625d212ef125701a
naruniyashan <naruniya5@gmail.com>
2022-01-16 17:15:01 +0800

    20220106 javascript 加入 jQuery 函式庫

--- javascript/main.js
+++ javascript/main.js
@@ -36,7 +36,6 @@ SmallNavCon();
 function SmallNavCon() {
     // 新增標籤 ul 與 li
     let ul = document.createElement('ul');
-    let li = document.createElement('li');
 
     // 在 SmallNav 內新增，宣告變數
     let SN = document.getElementById('SmallNav');
@@ -47,5 +46,57 @@ function SmallNavCon() {
     // 賦予屬性 id or class
     ul.setAttribute("id", "PhoneNav");
 
+    let PhoneNav = document.getElementById('PhoneNav');
     
+    let NavClass = ['About', 'VerticalLine', 'Shop', 'VerticalLine', 'message', 'VerticalLine', 'Address'];
+
+    let NavCon = ['品牌故事', '|', '購物說明', '|', '客服留言', '|', '門市資訊'];
+
+    // 新增標籤時，程式的運作方式為：先新增標籤，賦予屬性，再指定新增的外框架（此為一個迴圈）
+    // 迴圈內的數字 7 ，是因為陣列而產生，故要隨著陣列值的數量而自動增減
+    for ( let i = 0; i< NavClass.length; i++) {
+        let li = document.createElement('li');
+        // 新增屬性
+        li.setAttribute("class", NavClass[i]);
+        // ul 內新增 li 清單
+        PhoneNav.appendChild(li);
+
+        // 新增連結 a
+        let A = document.createElement('a');
+        // 新增屬性 假連結 javascript:;
+        A.setAttribute("href", "javascript:;");
+        // li 內新增 a 連結
+        li.appendChild(A);
+        // 置入 陣列內容
+        A.innerText = NavCon[i];
+    }
+}
+
+// MenuNav();
+// 行動裝置 選單
+function MenuNav() {
+    let MenuNav = document.getElementById('MenuNav');
+    // 隱藏 手機版選單
+    MenuNav.style.display = "none";
+}
+
+// PhoneMenushow();
+
+// menu 手機版按鈕
+function PhoneMenushow() {
+    let Navber = document.getElementById('Navber');
+    // Navber.getElementsByClassName('MenuBtn')[0].style.display = "";
+    let MenuBtn = Navber.getElementsByClassName('MenuBtn');
+    
+    // // 顯示 手機版選單內容
+    for ( let i = 0; i < MenuBtn.length; i++) {
+        MenuBtn[i].style.display = "";
+    }
+}
+
+
+
+Pageload();
+function Pageload() {
+    $('header').load("../web/header.html");
 }
---
+++ javascript/web.js
@@ -0,0 +1,16 @@
+PhoneMenushow2();
+function PhoneMenushow2() {
+    // $ jQuery的變數前綴詞
+    // 隱藏選單
+    $('#MenuNav').hide();
+
+    // 開啟選單
+    $('.MenuBtn').click( function() {
+        $('#MenuNav').show();
+    });
+
+    // 關閉選單
+    $('#Close').click( function() {
+        $('#MenuNav').hide();
+    });
+}
