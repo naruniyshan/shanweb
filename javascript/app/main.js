aebf1742c6f68f7a637a9cdef1b678fcb489a953
naruniyshan <naruniya5@gmail.com>
2022-03-20 14:05:17 +0800

    20220320 完成首頁 javascript 模組化設定

--- javascript/app/main.js
+++ javascript/app/main.js
@@ -1,11 +1,17 @@
 // 只要將檔名寫入陣列中（不用給副檔名），即可直接讀取檔案內容
-let IndexCon = ["jquery", "jquery.menunav", "copyright", "merchandise"];
+let IndexCon = ["jquery", "jquery.menunav", "copyright", "SmallNav", "Limited", "Share"];
+let FooterCon = ["copyright"];
 
 define(IndexCon, function($) {
-
     // 讀取 檔案內的 函式名稱
     $(function() {
         PhoneMenushow();
         CopyRightContent();
+        LimitedCon(8);
+        ShareCon(8);
     });
 });
+
+// define(FooterCon, function($){
+
+// });
